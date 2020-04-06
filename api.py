81fc8c08c3ba2dee2c95b9d6d9d01b28c09b3220
Cindy <ccnguyenn@gmail.com>
2020-04-06 14:49:05 -0700

    combine api files

--- api.py
+++ api.py
@@ -1,10 +1,28 @@
 import time
+import os
 from flask import Flask
-# from flask.ext.sqlalchemy import SQLAlchemy
+from flask_restful import Resource, Api
+from flask_pymongo import PyMongo
+from flask import make_response
+from bson.json_util import dumps
+
+MONGO_URL = os.environ.get('MONGO_URL')
+if not MONGO_URL:
+    MONGO_URL = "mongodb://localhost:27017/rest";
 
 app = Flask(__name__, static_folder='build', static_url_path='/')
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/template1'
-# db = SQLAlchemy(app)
+app.config['MONGO_URI'] = MONGO_URL
+mongo = PyMongo(app)
+
+def output_json(obj, code, headers=None):
+    resp = make_response(dumps(obj), code)
+    resp.headers.extend(headers or {})
+    return resp
+
+DEFAULT_REPRESENTATIONS = {'application/json': output_json}
+
+api = Api(app)
+api.representations = DEFAULT_REPRESENTATIONS
 
 @app.route('/')
 def index():
@@ -13,3 +31,5 @@ def index():
 @app.route('/api/time')
 def get_current_time():
 	return {'time': time.time()}
+
+import flask_rest_service.resources
